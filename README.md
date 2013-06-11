a332be6dce3e28bee065939be31b0d3a77b9483e
dameninngenn <dameninngenn.owata@gmail.com>
2013-06-12 02:40:23 +0900

    ブーーーーーン

---
+++ README.md
@@ -0,0 +1,9 @@
+chrome-ext-bu-------n
+========
+
+About:
+--------
+⊂二二二（ ＾ω＾）二⊃
+
+"Apache, Apache Hive, Hive, and the Hive yellow bee logo are either registered trademarks or trademarks of the Apache Software Foundation in the United States and/or other countries."
+
---
+++ src/popup.js
@@ -0,0 +1,40 @@
+restore_options();
+var btn = document.getElementById("save_btn");
+var btn_listener = function(e){
+    save_options();
+};
+btn.addEventListener('click', btn_listener, false);
+
+function save_options() {
+  var move_enabled = document.getElementById("move_enabled");
+  var move_enabled = move_enabled.checked == true ? 1 : 0;
+  var max_spawn = document.getElementById("max_spawn").value;
+  var data = {
+    bunbun : {
+      move_enabled : move_enabled,
+      max_spawn    : max_spawn ? max_spawn : 20
+    },
+  };
+
+  chrome.storage.local.set(data, function(){
+    var status = document.getElementById("status");
+    status.innerHTML = "Settings saved!!";
+    setTimeout(function() {
+      status.innerHTML = "";
+    }, 750);
+  });
+}
+
+function restore_options() {
+  document.getElementById("max_spawn").value = 20;
+  chrome.storage.local.get('bunbun', function(result){
+    var s_move_enabled = result.bunbun.move_enabled;
+    var move_enabled = document.getElementById("move_enabled");
+    move_enabled.checked = s_move_enabled == 1 ? true : false;
+
+    var s_max_spawn = result.bunbun.max_spawn;
+    if( s_max_spawn ) {
+      document.getElementById("max_spawn").value = s_max_spawn;
+    }
+  });
+}
